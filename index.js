8fdc56330ccf880c66b688235e897d5da4e999bc
Jonathan Chan <johnkchan252@gmail.com>
2018-11-20 04:11:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+var driver = {}
+
+function updateDriverWithKeyAndValue(driver, key, value){
+  driver[key] = value;
+  return driver
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+  driver[key] = value;
+  return driver;
+}
+
+function deleteFromDriverByKey(driver, key){
+  const newObj = { ...obj };
+ 
+  newObj[key] = value;
+ 
+  return newObj;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key){
+  
+}
